c76bac59a5860e53376b2b047261bb2fadc7fe15
Isidoro Rumeo <rumeoisidoro16@gmail.com>
2023-12-03 13:32:13 -0500

    Setting up

---
+++ main.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Dense, MaxPooling2D, Conv2D, Flatten
+from keras.optimizers import Adam
+import os
+import cv2
